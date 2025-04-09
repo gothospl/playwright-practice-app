161213535b56f6269df6119eb7609f610e9a5e35
gothospl <dziuba.bartosz@gmail.com>
2025-04-09 09:36:58 +0200

    After Checkboxes lesson

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -39,3 +39,25 @@ test.describe('Form Layouts page', () => {
         await expect(usingTheGridForm.getByRole('radio', {name: "Option 2"})).toBeChecked()
     })
 })
+
+test.describe('Toastr Page', () => {
+    test.beforeEach(async({page}) => {
+        await page.getByText('Modal & Overlays').click()
+        await page.getByText('Toastr').click()
+    })
+
+    test('checkboxes', async({page}) => {
+        await page.getByRole('checkbox', {name: "Hide on click"}).uncheck({force:true})
+        await page.getByRole('checkbox', {name: "Prevent arising of duplicate toast"}).check({force:true})
+
+        const allBoxes = page.getByRole('checkbox');
+        for(const box of await allBoxes.all()){
+            await box.check({force:true})
+            await expect(box).toBeChecked()
+        }
+        for(const box of await allBoxes.all()){
+            await box.uncheck({force:true})
+            await expect(box).not.toBeChecked()
+        }
+    })
+})
