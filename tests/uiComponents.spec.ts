0d94d07a606dcd63efadfa5dd670c6b513629962
gothospl <dziuba.bartosz@gmail.com>
2025-04-11 10:12:20 +0200

    Test Retries

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -5,24 +5,28 @@ test.beforeEach(async({page}, testInfo) => {
 })
 
 test.describe('Form Layouts page', () => {
+    test.describe.configure({retries: 4})
     test.beforeEach(async({page}) => {
         await page.getByText('Forms').click()
         await page.getByText('Form Layouts').click()
     })
 
-    test('input fields', async({page}) => {
+    test('input fields', async({page}, testInfo) => {
+        if(testInfo.retry){
+            //do something, like database cleanup
+        }
         const usingTheGridEmailInput = page.locator('nb-card ', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"})
 
         await usingTheGridEmailInput.fill("test@test.com")
         await usingTheGridEmailInput.clear()
-        await usingTheGridEmailInput.pressSequentially("test2@test.com", {delay: 500})
+        await usingTheGridEmailInput.pressSequentially("test2@test.com", {delay: 100})
 
         //generic assertion
         const inputValue = await usingTheGridEmailInput.inputValue()
         expect(inputValue).toEqual('test2@test.com')
 
         //locator assertion
-        await expect(usingTheGridEmailInput).toHaveValue('test2@test.com')
+        await expect(usingTheGridEmailInput).toHaveValue('test2@test.com1')
     })
 
     test('radio buttons', async({page}) => {
