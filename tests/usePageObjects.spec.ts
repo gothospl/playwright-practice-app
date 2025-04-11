3e44d32fd77f5b5b5729aac30c4d3d5351bd74ea
gothospl <dziuba.bartosz@gmail.com>
2025-04-11 09:59:29 +0200

    Test Data Generator

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import {test, expect} from '@playwright/test'
 import { PageManager } from '../page-objects/pageManager'
+import { faker } from '@faker-js/faker'    
 
 test.beforeEach(async({page}, testInfo) => {
     await page.goto('http:/localhost:4200')
@@ -17,10 +18,12 @@ test('navigate to form page', async({page}) => {
 
 test('parametrized methods', async({page}) => {
     const pm = new PageManager(page)
+    const randomFullName = faker.person.fullName({lastName: 'O\'Keefe'})
+    const randomEmail = `${randomFullName.replace(' ','').replace('\'', '')}${faker.number.int(1000)}@test.com`
 
     await pm.navigateTo().formLaoyutsPage()
     await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'password123', 'Option 2')
-    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCHeckbox('John Winchester', 'johnwin@test.com', false)
+    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCHeckbox(randomFullName, randomEmail, false)
     await pm.navigateTo().datepickerPage()
     await pm.onDatepickerPage().selectCommonDatepickerDateFromToday(69)
     await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(-69, 420)
