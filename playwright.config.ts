39ec3f8af2a27ac2e857906e654ff9db78b3d7ca
gothospl <dziuba.bartosz@gmail.com>
2025-04-14 11:50:40 +0200

    Reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,11 @@ export default defineConfig<TestOptions>({
   },
 
   retries: 1,
-  reporter: 'html',
+  reporter: [
+    ['json', {outputFile: 'test-results/jsonReport.json'}],
+    ['junit', {outputFile: 'test-results/junitReport.xml'}],
+    ['allure-playwright']
+],
 
   use: {
     baseURL: process.env.DEV == '1' ? 'http://localhost:4201/'
